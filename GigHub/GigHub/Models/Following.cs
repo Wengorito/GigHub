122572b37fdf963176a809a593a44f7c95eff916
Grzegorz Kowalski <gkowaalski@gmail.com>
2022-08-11 15:17:54 +0000

    Add Following to model. Imported from Changeset 19

--- GigHub/GigHub/Models/Following.cs
+++ GigHub/GigHub/Models/Following.cs
@@ -3,17 +3,18 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GigHub.Models
 {
-    public class Attendance
+    public class Following
     {
-        public Gig Gig { get; set; }
-        public ApplicationUser Attendee { get; set; }
+        public ApplicationUser Follower { get; set; }
+
+        public ApplicationUser Artist { get; set; }
 
         [Key]
         [Column(Order = 1)]
-        public int GigId { get; set; }
+        public string FollowerId { get; set; }
 
         [Key]
         [Column(Order = 2)]
-        public string AttendeeId { get; set; }
+        public string ArtistId { get; set; }
     }
 }
